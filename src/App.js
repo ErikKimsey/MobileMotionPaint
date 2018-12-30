2f8aab0ff1adcf3c0fc18956a786b3b4d9e3cab2
Erik Kimsey <erik@erikkimsey.com>
2018-12-30 17:40:38 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,45 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 class App extends Component {
+
+  componentDidMount(){
+    let app = document.querySelector('.App');
+    // console.log(app);
+    app.style.width = window.innerWidth;
+    app.style.height = window.innerHeight;
+    window.addEventListener("deviceorientation", this.handleOrientation, true);
+  }
+
+  moveIcon = (dimens) => {
+    let image = document.querySelector('img');
+    let iw = 604;
+    let ih = 412;
+    // console.log("height: ", ih);
+    console.log("dim alpha:", dimens.alpha);
+    console.log("dim beta:", dimens.beta);
+    console.log("dim gamma:", dimens.gamma);
+    
+    let xpos = (ih * dimens.alpha)/360;
+    // console.log("xpos: ", xpos);
+    
+    image.style.top = xpos + "px";
+  }
+
+  handleOrientation = (event) => {
+    // event.preventDefault();
+    // console.log(event);
+    
+    this.moveIcon(event);
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
